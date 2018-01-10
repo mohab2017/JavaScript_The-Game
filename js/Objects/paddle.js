a7f4354f50e1cc24b08b5808b630d034b8af951b
maisa9999 <34835579+maisa9999@users.noreply.github.com>
2018-01-10 21:44:05 +0200

    uudate
    update

--- js/Objects/paddle.js
+++ js/Objects/paddle.js
@@ -1,3 +1,51 @@
-function paddle() {
+var paddle=function (paddleWidth,paddleHeight,paddleX) {
+    
+    Object.defineProperty(this,'paddleWidth',{
+
+        get: function(){
+
+            return paddleWidth 
+        },
+        set: function(paddleWidth){            
+
+            paddleWidth = paddleWidth
+        }     
+    })
+
+    Object.defineProperty(this,'paddleHeight',{
+        get: function(){
+
+            return paddleHeight 
+        },
+        set: function(paddleHeight){            
+
+            paddleHeight = paddleHeight;
+        }     
+    });
+
+    Object.defineProperty(this,'paddleX',{
+        get: function(){
+
+            return paddleX 
+        },
+        set: function(paddleX){            
+
+            paddleX =paddleX 
+        }     
+    });
+
+ this.paddleWidth =  paddleWidth;
+ this.paddleHeight = paddleHeight;
+ this.paddleX = paddleX;
+
 
 }
+
+paddle.prototype.drawPaddle = function(){
+    ctx.beginPath();
+    ctx.rect(this.paddleX, canvas.height-this.paddleHeight, this.paddleWidth, this.paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
