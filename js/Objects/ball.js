5bbce37b9588767cabd37098253253d218bf4eee
Mohab <34834902+mohab2017@users.noreply.github.com>
2018-01-10 18:58:40 +0200

    Update ball.js

--- js/Objects/ball.js
+++ js/Objects/ball.js
@@ -1,3 +1,21 @@
-function ball(){
-
+function Ball(){
+    var ballx;
+    var bally;
+    var ballRadius;
+    var style;
+    var startAngle;
+    var endAngle=Math.PI*2;
+}
+Ball.prototype.drawBall(style){
+    ctx.beginPath();
+    ctx.arc(this.ballx, this.bally, this.ballRadius,this.startAngle,this.endAngle);
+    ctx.fillStyle = style;
+    ctx.fill();
+    ctx.closePath();
+}
+Ball.prototype.initiate=function(bx,by,br,stAngle){
+    this.ballx=bx;
+    this.bally=by;
+    this.startAngle=stAngle;
+    this.endAngle=Math.PI*2;
 }
