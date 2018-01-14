9e7a92dce8633b5445b813d19aa95e877e0bf124
maisa9999 <34835579+maisa9999@users.noreply.github.com>
2018-01-14 13:00:41 +0200

    update

--- js/MainActivity.js
+++ js/MainActivity.js
@@ -5,8 +5,8 @@ var timerId = 0;
     var welcomeImg=new Image();
      welcomeImg.src='img/c.jpg';
     ctx.drawImage(welcomeImg, 0, 0);
-    ctx.font = "30px Arial";
-    ctx.strokeText("Click on Screen",450,225); 
+    //ctx.font = "30px Arial";
+    //ctx.strokeText("Click on Screen",450,225); 
     canvas.addEventListener("click",displayMenu);
 /*  canvas.addEventListener("click",display);
   function display()
--- js/selectCharacter.js
+++ js/selectCharacter.js
@@ -2,7 +2,9 @@ function selectCharacter() {
     clearInterval(timerId);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //canvas.removeEventListener("click", selectCharacter);
-    ctx.drawImage(welcomeImg, 0, 0);
-    ctx.font = "30px Arial";
-    ctx.strokeText("Click on Screen", 450, 225);
+    var newimg = new Image();
+    newimg.src = 'img/play.png'
+    ctx.drawImage(newimg, 0, 0);
+    //ctx.font = "30px Arial";
+    //ctx.strokeText("Click on Screen", 450, 225);
 }
